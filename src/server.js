aac052f89199e339c037490076241ed7397b2448
vito madio <vitomadio@hotmail.com>
2019-09-13 15:03:28 +0200

    I think am almost done with this shit

--- src/server.js
+++ src/server.js
@@ -78,7 +78,6 @@ function htmlTemplate(reactDom, reduxState, helmetData) {
         <html>
         <head>
             <meta charset='utf-8'>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             ${ helmetData.title.toString()}
             ${ helmetData.meta.toString()}
             ${ helmetData.link.toString()}
