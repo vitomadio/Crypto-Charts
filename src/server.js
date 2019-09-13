585f9ed2ddd2f3ccd4530123b724c9e5e4272f0e
vito madio <vitomadio@hotmail.com>
2019-09-13 14:32:02 +0200

    adding links to the header on server file

--- src/server.js
+++ src/server.js
@@ -78,6 +78,8 @@ function htmlTemplate(reactDom, reduxState, helmetData) {
         <html>
         <head>
             <meta charset='utf-8'>
+            <link href="stylesheet" href="css/bootstrap.min.css">
+            el="shorcut icon" href="assets/CC.png" type="image/x-icon" />
             ${ helmetData.title.toString()}
             ${ helmetData.meta.toString()}
             ${ helmetData.link.toString()}
